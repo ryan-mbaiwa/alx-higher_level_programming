788e200aef4e65db57e0dfcdc226e2b1dba20b22
ryan <ryanmbaiwa@gmail.com>
2023-01-16 03:43:13 +0200

    reduce length on module pep8 style

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 
-This module  tests if the object is exactly an instance 
+This module  object is exactly an instance 
 of a class that Inherited from the specified class
 
 """
