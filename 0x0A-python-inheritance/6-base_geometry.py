65982c45edc9777ce767eef4cf736c4f43ee408b
ryan <ryanmbaiwa@gmail.com>
2023-01-16 03:59:19 +0200

    pep8 trailiing spaces fixed

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 
-A module based on a class that raises an exception 
+A module based on a class that raises an exception
 
 """
 
 
 class BaseGeometry:
     """empty class """
-    
+
     def area(self):
         raise Exception("area() is not implemented")
